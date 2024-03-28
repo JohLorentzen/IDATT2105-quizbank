9394c75f964fa11cb9e61999a5449f3682164e3b
Johannes <johalor@stud.ntnu.no>
2024-03-28 09:46:55 +0100

    feat: create new quiz; fields for quiz

--- frontend/src/views/CreateEdit.vue
+++ frontend/src/views/CreateEdit.vue
@@ -1,21 +1,43 @@
 <script setup>
 import QuizConfig from '@/components/QuizConfig.vue';
-import { ref } from 'vue'; 
+import QuizGrid from '@/components/QuizGrid.vue';
+import axios from 'axios';
+import { onMounted, ref } from 'vue'; 
+import { useRouter } from 'vue-router';
 
-const newQuiz = ref(null)
 
-const submitQuiz = (quiz) => {
-    newQuiz.value = quiz;
-}
+const path = ref('create');
+const currentQuiz = ref(null);
+const quizes = ref([]);
+const router = useRouter();
+
+const fetchQuizes = async () => {
+  try {
+    const response = await axios.get('http://localhost:8080/rest/quiz/1', {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    });
+    quizes.value = response.data;
+  } catch (error) {
+    if (error.response && [401, 404].includes(error.response.status)) {
+      router.push('/login');
+    } else {
+      console.error('Error fetching quizzes:', error);
+    }
+  }
+};
+
+onMounted(fetchQuizes);
 </script>
 <template>
-    
-    <div>
-        <h1>Create or edit your quiz</h1>
-        <QuizConfig @submitQuiz="submitQuiz"/>
-    </div>
     <div> 
          <button>Create quiz</button>
         <button>Edit quiz</button>
     </div>
+    <div>
+        <h1>Create or edit your quiz</h1>
+        <QuizGrid v-if="!currentQuiz" :quizes="quizes" @selectQuiz="currentQuiz = $event"/>
+    </div>
+    
 </template>
