efc1024e9937e5d1440cf86df52cc5d3f3d88902
jensjordal <jens.jordal@hotmail.no>
2024-04-01 13:32:57 +0200

    fix: mocking validation for tests

--- src/test/java/quizbank/controller/QuizControllerTest.java
+++ src/test/java/quizbank/controller/QuizControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,18 +18,21 @@ class QuizControllerTest {
     private MockMvc mockMvc;
 
     @Test
+    @WithMockUser
     void getAllQuizzes() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/rest/quiz"))
                 .andExpect(status().isOk());
     }
 
     @Test
+    @WithMockUser
     void getQuiz() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/rest/quiz/1"))
                 .andExpect(status().isOk());
     }
 
     @Test
+    @WithMockUser
     void deleteQuiz() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.delete("/rest/quiz/1"))
                 .andExpect(status().isNoContent());
