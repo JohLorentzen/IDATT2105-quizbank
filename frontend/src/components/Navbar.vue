2652ef9b48c277575e174de2a51775a2ab2246f3
sondrefjellving <sondrefjellving@gmail.com>
2024-04-04 10:53:29 +0200

    feat: Updated base styling and styled header to fit mobile screen

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -22,60 +22,50 @@ import {RouterView, RouterLink, useRouter} from 'vue-router'
 
 </script>
 <template>
-    <div class="navbar">
+    <header class="navbar">
         <nav>
           <RouterLink to="/">Home</RouterLink>
           <RouterLink to="/quiz">Play quiz</RouterLink>
           <RouterLink to="/about">About</RouterLink>
           <RouterLink to="/createEdit">Your quizes</RouterLink>
-      </nav>
-      <nav class="login">
-            <RouterLink to="/login" @click="handleLogout">{{ loggedInStatus }}</RouterLink>
-      </nav>
-    </div>
+          <RouterLink class="login" to="/login" @click="handleLogout">{{ loggedInStatus }}</RouterLink>
+        </nav>
+    </header>
 </template>
 <style scoped>
 .navbar {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
   position: fixed;
-  top: 0;
-  left: 0;
   width: 100%;
-  padding: 15px 10%;
-  background-color: #f0f2f5;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-  border-bottom: 1px solid #d3d4d8;
-  z-index: 1000;
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  align-items: center;
+  background: var(--bg-white-soft);
 }
 
-.navbar nav a {
-  color: #333;
-  text-decoration: none;
-  padding: 10px 15px;
-  border-radius: 5px;
-  transition: background-color 0.3s ease;
-}
-
-.navbar nav a:hover,
-.navbar nav a:focus {
-  background-color: #e1e3e6;
+nav {
+  grid-column: 2 / -2;
+  display: flex;
+  gap: 1em;
 }
 
 .login {
   margin-left: auto;
 }
 
-/* Responsive adjustments */
-@media (max-width: 768px) {
-  .navbar {
-    padding: 10px 5%; /* Smaller padding on smaller screens */
-  }
+a {
+  color: var(--text-color);
+  text-decoration: none;
+  padding: 0.5em 0;
+  font-size: 0.8rem;
+}
 
-  .navbar nav a {
-    padding: 8px 10px; /* Adjust link padding on smaller screens */
-  }
+a:hover,
+a:active,
+a.active {
+  background: none;
+  padding-bottom: 0.4em;
+  border-bottom: 0.1em solid var(--bg-light-blue);
+  cursor: pointer;
 }
 </style>
 
