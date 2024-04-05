c4098f9c9b4afe34f8c1f05c25c36b0e964e4e2e
jensjordal <jens.jordal@hotmail.no>
2024-04-05 12:46:51 +0200

    feat: possible to share quiz in gui - styling

--- frontend/src/views/CreateEdit.vue
+++ frontend/src/views/CreateEdit.vue
@@ -4,8 +4,8 @@ import QuizGrid from '@/components/QuizGrid.vue';
 import EditQuiz from '@/components/EditQuiz.vue';
 import endpoints from '@/endpoints.json';
 import axios from 'axios';
-import { onMounted, ref } from 'vue'; 
-import { useRouter } from 'vue-router';
+import {onMounted, ref} from 'vue';
+import {useRouter} from 'vue-router';
 
 const router = useRouter();
 const quizzes = ref([]);
@@ -14,10 +14,10 @@ const currentQuiz = ref(null);
 const deleteMode = ref(false);
 
 const fetchQuizes = async () => {
-  
+
   const url = `${endpoints.BASE_URL}${endpoints.GET_ALL_QUIZZES}`;
   try {
-    const response = await axios.get( url, {
+    const response = await axios.get(url, {
       headers: {
         Authorization: `Bearer ${localStorage.getItem('token')}`,
       },
@@ -33,24 +33,24 @@ const fetchQuizes = async () => {
 };
 
 const postQuiz = async (quizData) => {
-  
+
   const url = `${endpoints.BASE_URL}${endpoints.GET_ALL_QUIZZES}`
-    try {
-        await axios.post(url, quizData, {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`,
-            },
-        });
-        createQuiz.value = false;
-        fetchQuizes();
-    } catch (error) {
-        console.error('Error posting quiz:', error);
-    }
+  try {
+    await axios.post(url, quizData, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    });
+    createQuiz.value = false;
+    fetchQuizes();
+  } catch (error) {
+    console.error('Error posting quiz:', error);
+  }
 };
 
 const handleQuizSubmit = (quizData) => {
-    postQuiz(quizData);
-    router.push('/createEdit');
+  postQuiz(quizData);
+  router.push('/createEdit');
 };
 
 const toggleDeleteMode = () => {
@@ -59,10 +59,10 @@ const toggleDeleteMode = () => {
 const selectQuiz = (quiz) => {
   if (deleteMode.value) {
     const isConfirmed = confirm(`Are you sure you want to delete the quiz "${quiz.quizName}"?`);
-    if (isConfirmed){
+    if (isConfirmed) {
       deleteQuiz(quiz.quizId);
     }
-    deleteMode.value = false; 
+    deleteMode.value = false;
   } else {
     currentQuiz.value = quiz;
   }
@@ -70,36 +70,38 @@ const selectQuiz = (quiz) => {
 
 const deleteQuiz = async (quizId) => {
   const url = `${endpoints.BASE_URL}${endpoints.GET_ALL_QUIZZES}/${quizId}`;
-    try {
-        await axios.delete( url, {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`,
-            },
-        });
-        fetchQuizes();
-    } catch (error) {
-        console.error('Error deleting quiz:', error);
-    }
+  try {
+    await axios.delete(url, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    });
+    fetchQuizes();
+  } catch (error) {
+    console.error('Error deleting quiz:', error);
+  }
 };
 
 onMounted(fetchQuizes);
 </script>
 <template>
-  <h1>Quizes</h1>
-  <div v-if="createQuiz">
-    <CreateQuiz @submitQuiz="handleQuizSubmit"/>
-  </div>
-  <div v-else>
+  <div class="create-edit">
+    <h1>Quizes</h1>
+    <div v-if="createQuiz">
+      <CreateQuiz @submitQuiz="handleQuizSubmit"/>
+    </div>
+    <div v-else>
       <button @click="createQuiz = true"> Create quiz</button>
       <button @click="toggleDeleteMode">Delete quiz</button>
       <QuizGrid v-show="!showModal" :quizzes="quizzes" @selectQuiz="selectQuiz"/> <!-- Use v-show instead of v-if -->
-  </div>
-  <div v-if="currentQuiz">
+    </div>
+    <div v-if="currentQuiz">
       <EditQuiz :quiz="currentQuiz" @submit="handleQuizSubmit"/>
+    </div>
   </div>
 </template>
-<style>
-div {
-  margin: 20px;
+
+<style scoped>
+.create-edit {
 }
 </style>
