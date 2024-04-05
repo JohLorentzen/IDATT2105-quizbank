dbbc5fc813e31c5275c444a4be10cf9f6b394dad
Johannes <johalor@stud.ntnu.no>
2024-04-05 19:02:17 +0200

    fix: added notfound changes to router links

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -4,8 +4,8 @@
       <h2>404 - Page Not Found</h2>
       <p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
       <div class="action-buttons">
-        <router-link to="/contact" class="report-link">Report a bug</router-link>
         <router-link to="/" class="home-link">Back to safe grounds</router-link>
+        <router-link to="/contact" class="report-link">Report a bug</router-link>
       </div>
     </div>
   </template>
