bde92a6905b728578da66be3659c319cc86ad0e7
sondrefjellving <sondrefjellving@gmail.com>
2024-04-06 14:14:46 +0200

    feat: Updated styling on quiz cards and share modal

--- frontend/src/components/QuizGrid.vue
+++ frontend/src/components/QuizGrid.vue
@@ -5,6 +5,7 @@ import ShareModal from './ShareModal.vue';
 const props = defineProps(['quizzes']);
 const emit = defineEmits(['selectQuiz']);
 const currentQuiz = ref(null);
+const sharedQuiz = ref(null);
 const showModal = ref(false);
 
 const availableQuizzesTitle = computed(() => {
@@ -21,8 +22,13 @@ function playQuiz(quiz) {
 }
 
 function shareQuiz(quiz) {
-  currentQuiz.value = quiz;
+  sharedQuiz.value = quiz;
   showModal.value = true;
+}
+
+function closeModal() {
+  sharedQuiz.value = null;
+  showModal.value = false;
 }
 </script>
 
@@ -32,11 +38,13 @@ function shareQuiz(quiz) {
     <div class="quiz-card" v-for="quiz in quizzes" :key="quiz.quizId" @click="playQuiz(quiz)">
       <h2>{{ quiz.quizName }}</h2>
       <p class="category">{{ quiz.category }}</p>
-      <p class="questions">{{ quiz.questions ? `${quiz.questions.length} questions` : 'No questions' }}</p>
-      <button @click.stop="shareQuiz(quiz)" class="share-button">Share</button>
+      <div class="questions-and-share">
+        <button @click.stop="shareQuiz(quiz)" class="share-button">Share</button>
+        <p class="questions">{{ quiz.questions ? `${quiz.questions.length} questions` : 'No questions' }}</p>
+      </div>
     </div>
   </div>
-  <ShareModal v-if="showModal" :quiz="currentQuiz" @close="showModal = false" />
+  <ShareModal v-if="showModal" :quiz="sharedQuiz" @close="closeModal" />
 </template>
 
 
@@ -55,7 +63,7 @@ h1 {
   margin-top: 1em;
   grid-column: 2 / -2;
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(160px, 1fr));
   grid-auto-rows: 120px;
   gap: 2em;
 }
@@ -66,6 +74,7 @@ h1 {
   display: flex;
   flex-direction: column;
   align-items: start;
+  justify-content: space-between;
   border-radius: 0.8em;
   border: 1px solid var(--border-very-light-blue);
 }
@@ -84,22 +93,46 @@ h1 {
 }
 
 .category {
-  margin-top: 0.8em;
   font-size: 0.54rem;
   padding: 0.5em 1em;
-  border-radius: 1em;
+  border-radius: 0.4em;
   font-weight: bold;
-  color: white;
-  background-color: var(--bg-light-blue);
+  color: var(--text-color-light-grey);
+  border: 1px solid var(--text-color-light-grey);
+}
+
+.questions-and-share {
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+  width: 100%;
 }
 
 p.questions {
-  margin-top: auto;
-  font-size: 0.85rem;
+  font-size: 0.70rem;
   color: var(--text-color-grey);
 }
 
+.share-button {
+   border: none;
+   padding: 0.5em 1em;
+   border-radius: 1.4em;
+   background: var(--button-bg-strong-blue);
+   font-weight: bold;
+   color: white;
+   font-size: 0.8rem;
+   cursor: pointer;
+ }
+
+.share-button:hover {
+  background: var(--button-bg-hover-blue);
+}
+
 @media (min-width: 720px) {
+  .quiz-grid {
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  }
+
   h1 {
     font-size: 1.1rem;
   }
@@ -117,11 +150,20 @@ p.questions {
   }
 
   p.questions {
-    font-size: 1rem;
+    font-size: 0.9rem;
+  }
+
+  .share-button {
+    font-size: 0.9rem;
+    padding: 0.6em 1.2em;
+    border-radius: 2.4em;
   }
 }
 
 @media (min-width: 1024px) {
+  .quiz-grid {
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  }
   .quiz-grid h2 {
     font-size: 1.6rem;
   }
@@ -131,24 +173,7 @@ p.questions {
   }
 
   p.questions {
-    font-size: 1.125rem;
+    font-size: 1rem;
   }
 }
-
-.share-button {
-  margin-top: 1em;
-  border: none;
-  padding: 0.5em 1em;
-  border-radius: 1em;
-  background: var(--button-bg-strong-blue);
-  font-weight: bold;
-  color: white;
-  font-size: 0.8rem;
-  cursor: pointer;
-}
-
-.share-button:hover {
-  background: var(--button-bg-hover-blue);
-}
-
 </style>
