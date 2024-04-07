ba4eaefc7f90a748f858419e92fa3f2b20a29afb
Jens Jordal <jens.jordal@postnord.no>
2024-04-07 10:43:26 +0200

    chore: documented jpa repositories

--- src/main/java/quizbank/repository/QuestionRepository.java
+++ src/main/java/quizbank/repository/QuestionRepository.java
@@ -3,5 +3,8 @@ package quizbank.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import quizbank.model.Question;
 
+/**
+ * Repository for managing {@link Question} entities.
+ */
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 }
